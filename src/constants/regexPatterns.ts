34e9b933af6e41450de663b9a620f4e6d429771a
drumil32 <akheniad@gmail.com>
2024-07-23 21:13:40 +0530

    password regex updated

--- src/constants/regexPatterns.ts
+++ src/constants/regexPatterns.ts
@@ -1,6 +1,6 @@
 export const nameRegex = /^[a-zA-Z]{2,50}$/;
 export const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-export const passwordRegex = /^.{4,9}$/;
+export const passwordRegex = /^.{4,16}$/;
 export const pincodeRegex = /^\d{6}$/;
 export const phoneRegex = /^\d{10}$/;
 export const addressRegex = /^[a-zA-Z ]{2,50}$/;
