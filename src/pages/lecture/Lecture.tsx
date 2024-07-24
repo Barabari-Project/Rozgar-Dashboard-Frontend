9686294e738549cb04c2f3080d2620315728e07b
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-07-24 08:19:00 +0530

    adding content link section

--- src/pages/lecture/Lecture.tsx
+++ src/pages/lecture/Lecture.tsx
@@ -50,7 +50,7 @@ const LectureDashboard: React.FC = () => {
         {/* left */}
         <div
           id="left"
-          className="lg:max-h-[100vh] pt-7 pb-4 px-1 sm:px-2 md:px-4 lg:px-5 w-full lg:w-3/4 bg-white rounded-xl shadow-lg"
+          className="lg:max-h-[108vh] pt-7 pb-4 px-1 sm:px-2 md:px-4 lg:px-5 w-full lg:w-3/4 bg-white rounded-xl shadow-lg"
         >
           <h1 className="border-l-8 text-[#324498] font-semibold text-xl font-semifold border-[#FFCB33] bg-white px-4 py-1">
             {module?.title}
@@ -59,12 +59,12 @@ const LectureDashboard: React.FC = () => {
           {/* tab */}
           <div
             id="tabs"
-            className="tab-container flex items-center w-full h-[40px] mt-2 pb-1 border-b-2 rounded-xl border-gray-400"
+            className="tab-container flex items-center w-full h-[40px] mt-2 pb-1 border-b-2 rounded-xl border-gray-400 px-1 overflow-hidden"
           >
             <div
               className={`${tab === LecturePageTab.Video
-                ? "bg-[#fee496]"
-                : ""
+                ? "bg-[#fee496] text-black"
+                : "text-[#595959]"
                 } px-5 h-full flex items-center hover:cursor-pointer duration-500 rounded-xl`}
               onClick={() => setTab(LecturePageTab.Video)}
             >
@@ -74,8 +74,8 @@ const LectureDashboard: React.FC = () => {
 
             <div
               className={`${tab === LecturePageTab.Question
-                ? "bg-[#fee496]"
-                : ""
+                ? "bg-[#fee496] text-black"
+                : "text-[#595959]"
                 } px-5 h-full flex items-center hover:cursor-pointer duration-500 rounded-xl`}
               onClick={() => setTab(LecturePageTab.Question)}
             >
@@ -83,8 +83,8 @@ const LectureDashboard: React.FC = () => {
             </div>
             <div
               className={`${tab === LecturePageTab.Assignment
-                ? "bg-[#fee496]"
-                : ""
+                ? "bg-[#fee496] text-black"
+                : "text-[#595959]"
                 } px-5 h-full flex items-center hover:cursor-pointer duration-500 rounded-xl`}
               onClick={() => setTab(LecturePageTab.Assignment)}
             >
@@ -92,7 +92,7 @@ const LectureDashboard: React.FC = () => {
             </div>
           </div>
 
-          <div className="max-h-[88%] overflow-scroll overflow-y-auto">
+          <div className="max-h-[90%] overflow-scroll overflow-y-auto">
           {tab === LecturePageTab.Video ? (
             <VideoTab topic={activeTopic} />
           ) : tab === LecturePageTab.Assignment ? (
@@ -102,12 +102,17 @@ const LectureDashboard: React.FC = () => {
           )}
           </div>
 
+          <div className="w-full border-t-2 border-grey-400 py-2 lg:py-4">
+            <h2 className="text-start text-2xl font-semibold px-2">Content</h2>
+            <span className="flex items-center px-4">➡️<a href="#" className="text-[#324498] underline">Link</a></span>
+          </div>
+
         </div>
 
         {/* right */}
         <div
           id="Right"
-          className="w-full max-h-[100vh] overflow-hidden lg:w-1/4 bg-white rounded-xl shadow-lg"
+          className="w-full max-h-[108vh] overflow-hidden lg:w-1/4 bg-white rounded-xl shadow-lg"
         >
           {/* Module Heading */}
           <div className="flex gap-4 items-center px-4 pt-3 text-md text-ellipsis xl:pt-5 pb-3 hover:bg-[rgb(245,235,235)] shadow-md hover:cursor-pointer duration-300">
@@ -118,7 +123,7 @@ const LectureDashboard: React.FC = () => {
           </div>
 
           {/* Lecture List */}
-          <div className="flex flex-col overflow-scroll max-h-[93%]">
+          <div className="flex flex-col lg:overflow-scroll max-h-[93%] w-full">
             {module?.topics.map((topic) => (
               <div
                 onClick={() => {
@@ -127,7 +132,7 @@ const LectureDashboard: React.FC = () => {
                 }}
                 key={topic._id}
                 className={`${topic._id === activeTopic?._id ? "bg-[#ffecb3]" : ""
-                  } flex gap-4 items-center px-4 py-5 border-grey-300 border-t-[1px] border-b-[1px] hover:bg-[#ffecb3] hover:cursor-pointer duration-300`}
+                  } flex gap-4 w-full items-center px-4 py-5 border-grey-300 border-t-[1px] border-b-[1px] hover:bg-[#ffecb3] hover:cursor-pointer duration-300`}
               >
                 <div className="flex items-center gap-3">
                   <label className="text-white">
