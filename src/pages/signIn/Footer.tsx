50ff1d43c672018cf7db806450636b81b367e93b
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-08-04 16:00:08 +0530

    logos sliding animation is pending

--- src/pages/signIn/Footer.tsx
+++ src/pages/signIn/Footer.tsx
@@ -13,11 +13,28 @@ import { nanoid } from '@reduxjs/toolkit';
 
 const Footer: React.FC = () => {
 
-  const logos = [logo1, logo2, logo3, logo4, logo5, logo6, logo7,logo1, logo2, logo3, logo4, logo5, logo6, logo7,logo1, logo2, logo3, logo4, logo5, logo6, logo7];
+  // const logos = [logo1, logo2, logo3, logo4, logo5, logo6, logo7];
   return (
-    <div className="marquee h-[200px] lg:h-[250px]">
-      <div className="marquee-content">
-        <div className='flex gap-20'>{logos.map(img => (<img src={img} key={nanoid()} className='h-[90px]' />))}</div>
+    <div className="logos h-[200px] lg:h-[250px]">
+      <div className="logos-slide">
+        {/* {logos.map(img => (<img src={img} key={nanoid()} className='h-[90px]' />))} */}
+        <img src={logo1} className='h-[90px]' />
+        <img src={logo2} className='h-[90px]' />
+        <img src={logo3} className='h-[90px]' />
+        <img src={logo4} className='h-[90px]' />
+        <img src={logo5} className='h-[90px]' />
+        <img src={logo6} className='h-[90px]' />
+        <img src={logo7} className='h-[90px]' />
+      </div>
+      <div className="logos-slide">
+        {/* {logos.map(img => (<img src={img} key={nanoid()} className='h-[90px]' />))} */}
+        <img src={logo1} className='h-[90px]' />
+        <img src={logo2} className='h-[90px]' />
+        <img src={logo3} className='h-[90px]' />
+        <img src={logo4} className='h-[90px]' />
+        <img src={logo5} className='h-[90px]' />
+        <img src={logo6} className='h-[90px]' />
+        <img src={logo7} className='h-[90px]' />
       </div>
     </div>
   );
